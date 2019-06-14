a0823c998f2b1ab1a5d0e7bcc76a534736769948
ulquiorra7 <ciferulquiorra008@gmail.com>
2019-06-14 22:10:38 +0545

    SearchView added in search fragment

--- app/src/main/java/com/example/carrental/Fragments/Search.java
+++ app/src/main/java/com/example/carrental/Fragments/Search.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 import com.example.carrental.R;
 
@@ -19,12 +20,12 @@ public class Search extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view=inflater.inflate(R.layout.fragment_search, container, false);
+
         return view;
     }
 
