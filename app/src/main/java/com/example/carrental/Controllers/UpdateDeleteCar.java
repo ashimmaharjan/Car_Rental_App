86eb6b8cc8a33e6c070049320152d16e86add5be
ulquiorra7 <ciferulquiorra008@gmail.com>
2019-06-28 22:06:13 +0545

    Id of UI of update and delete car called in respective java file

--- app/src/main/java/com/example/carrental/Controllers/UpdateDeleteCar.java
+++ app/src/main/java/com/example/carrental/Controllers/UpdateDeleteCar.java
@@ -2,14 +2,33 @@ package com.example.carrental.Controllers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 import com.example.carrental.R;
 
 public class UpdateDeleteCar extends AppCompatActivity {
 
+    EditText searchCar,carName,carMan,carSeats,carMileage,carRPrice;
+    Button btnSearch,btnUpdate,btnDelete;
+    RadioGroup AC_Status;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_delete_car);
+
+        searchCar=findViewById(R.id.etSearch);
+        carName=findViewById(R.id.fillCarName);
+        carMan=findViewById(R.id.fillCarMan);
+        carSeats=findViewById(R.id.fillCarSeats);
+        carMileage=findViewById(R.id.fillCarMileage);
+        carRPrice=findViewById(R.id.fillCarPrice);
+
+        btnSearch=findViewById(R.id.searchCar);
+        btnUpdate=findViewById(R.id.updateCar);
+        btnDelete=findViewById(R.id.deleteCar);
     }
 }
