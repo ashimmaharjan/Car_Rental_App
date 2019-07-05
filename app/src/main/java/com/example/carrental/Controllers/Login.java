f087955951beeca4968166b919088d09711b9881
ulquiorra7 <ciferulquiorra008@gmail.com>
2019-07-05 23:06:04 +0545

    Proxomity sensor used to turn on/off screen based on the distance of an object from the screen

--- app/src/main/java/com/example/carrental/Controllers/Login.java
+++ app/src/main/java/com/example/carrental/Controllers/Login.java
@@ -2,11 +2,16 @@ package com.example.carrental.Controllers;
 
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.net.ConnectivityManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -32,6 +37,8 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
     Button btnLogin,btnSignUp;
     CarRentalsAPI api;
 
+    private SensorManager sensorManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,12 +50,14 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         btnSignUp=findViewById(R.id.signUp);
         btnLogin.setOnClickListener(this);
         btnSignUp.setOnClickListener(this);
+        proximity();
 
         Retrofit retrofit=new Retrofit.Builder()
                 .baseUrl("http://10.0.2.2:6969/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         api=retrofit.create(CarRentalsAPI.class);
+
     }
 
     @Override
@@ -121,5 +130,42 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         super.onStop();
 
         unregisterReceiver(broadcastReceiver);
+    }
+
+    public void proximity()
+    {
+        sensorManager=(SensorManager)getSystemService(SENSOR_SERVICE);
+        Sensor sensor=sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        if (sensor == null)
+        {
+            Toast.makeText(this, "No sensor detected", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(this, "Sensor Kicking in .....", Toast.LENGTH_SHORT).show();
+        }
+
+        SensorEventListener proximityListener=new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                WindowManager.LayoutParams params = Login.this.getWindow().getAttributes();
+                if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
+                    if (event.values[0] == 0) {
+                        params.flags = WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON;
+                        params.screenBrightness = 0;
+                        getWindow().setAttributes(params);
+                    } else {
+                        params.flags = WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON;
+                        params.screenBrightness = -1f;
+                        getWindow().setAttributes(params);
+                    }
+                }
+            }
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+        sensorManager.registerListener(proximityListener,sensor,SensorManager.SENSOR_DELAY_FASTEST);
     }
 }
